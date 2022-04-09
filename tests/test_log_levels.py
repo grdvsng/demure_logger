c535d775c028b993bf57ab59223315297dce86a3
grdvsng <grdvsng@gmail.com>
2022-04-09 15:35:19 +0400

    123

---
+++ tests/test_log_levels.py
@@ -0,0 +1,32 @@
+import unittest
+
+from src.demure_logger.log            import Levels
+from src.demure_logger.log.exceptions import LogLevelsDigestReadOnlyExists, LevelDoesntExists
+
+
+class Test( unittest.TestCase ):
+    def test_get( self ):
+        self.assertEqual( Levels.ignore, 0 ) 
+        self.assertEqual( Levels.fatal , 1 )  
+        self.assertEqual( Levels.ignOre, 0 ) 
+        self.assertEqual( Levels.fAtal , 1 ) 
+
+        self.assertRaises( LevelDoesntExists, lambda : Levels.test )
+
+    def test_cmp( self ):
+        self.assertEqual( Levels.ignore, Levels.ignore )
+        self.assertLess ( Levels.ignore, Levels.fatal  )
+
+        self.assertGreater( Levels.fatal , Levels.ignore )
+
+        self.assertRaises( TypeError, lambda : Levels.fatal >= "133" )
+
+    def test_set( self ):
+        def _( ) : 
+            Levels.test = 123
+
+        self.assertRaises( LogLevelsDigestReadOnlyExists, _ )
+
+
+if __name__ == '__main__':
+    unittest.main( )
